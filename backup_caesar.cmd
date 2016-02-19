2b0110cb57dd29241f8f617f6e31aa1ee3e4d65f
Radim Klaška <radim@klaska.net>
2016-02-19 17:05:55 +0100

    Original version of backup_caesar.cmd I wrote few years back.

---
+++ backup_caesar.cmd
@@ -0,0 +1,38 @@
+:: time and date stamp YYYYMMDD, HHMMSS and YYYY-MM-DD_HH-MM-SS
+@echo off
+for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set dt=%%a
+set datestamp=%dt:~0,8%
+set timestamp=%dt:~8,6%
+set YYYY=%dt:~0,4%
+set MM=%dt:~4,2%
+set DD=%dt:~6,2%
+set HH=%dt:~8,2%
+set Min=%dt:~10,2%
+set Sec=%dt:~12,2%
+
+set stamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%
+
+echo ===
+echo ===
+echo ===
+echo !!! Vytvarim snapshot v case: "%stamp%"
+echo ===
+echo ===
+echo ===
+md "C:\Documents and Settings\Uzivatel\Dokumenty\Dropbox\Zalohy_CEZAR\snapshot_%stamp%\"
+echo !!! Zaloha bude ulozena zde: "C:\Documents and Settings\Uzivatel\Dokumenty\Dropbox\Zalohy_CEZAR\snapshot_%stamp%"
+echo ===
+echo ===
+echo ===
+echo !!! KOPIRUJI! NEVYPINAT!
+xcopy "C:\CEZAR\*" "C:\Documents and Settings\Uzivatel\Dokumenty\Dropbox\Zalohy_CEZAR\snapshot_%stamp%\" /s /i /Y
+echo !!! KOPIROVANI DOKONCENO.
+echo ===
+echo ===
+echo ===
+echo !!! Prosim vyckejce na DOKONCENI synchronizace Dropboxu!
+echo !!! (Toto okno lze jiz zavrit libovolnou klavesou.)
+echo ===
+echo ===
+echo ===
+pause
